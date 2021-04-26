e791db26693af802ca80c0c7813b560b8d6a35ae
Shkredov Dmitriy <ShkredovDmitriy@gmail.com>
2021-04-26 09:07:02 +0300

    change folder name

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,11 +1,11 @@
-const dotenv = require('./_modules/_dotenv');                  // get data from .env to process.env.SOME_VAR
+const dotenv = require('./webpack/_dotenv');                  // get data from .env to process.env.SOME_VAR
 const { merge } = require('webpack-merge');                    // merge some webpack plugins
-const modeDev = require('./_modules/_modeDev');                // webpack mode, entry and out points
-const devServer = require('./_modules/_devServer');            // local http server http://localhost:8080
-const tsLoaderDev = require('./_modules/_tsLoaderDev');        // transform ts to js, use babel, insert to head
-const pugLoader = require('./_modules/_pugLoader');            // transform pug to html, export to index.html
-const scssLoaderDev = require('./_modules/_scssLoaderDev');    // autoprefix styles, insert to head
-const copyStaticDev = require('./_modules/_copyStaticDev');        // copy images, fonts, docs from src to temp
+const modeDev = require('./webpack/_modeDev');                // webpack mode, entry and out points
+const devServer = require('./webpack/_devServer');            // local http server http://localhost:8080
+const tsLoaderDev = require('./webpack/_tsLoaderDev');        // transform ts to js, use babel, insert to head
+const pugLoader = require('./webpack/_pugLoader');            // transform pug to html, export to index.html
+const scssLoaderDev = require('./webpack/_scssLoaderDev');    // autoprefix styles, insert to head
+const copyStaticDev = require('./webpack/_copyStaticDev');        // copy images, fonts, docs from src to temp
 
 module.exports = env => {
   return merge([
---
+++ src/app/modules/_constants/constants.js
@@ -0,0 +1,29 @@
+const constants = {
+  // MODALS
+  modal: {
+    reg: "#modalReg",
+    login: "#modalLogin",
+  },
+  form: {
+    // FORM REGISTRATION
+    reg: {
+      id: "#formReg",
+      name: "#formRegName",
+      name_last: "#formRegLastName",
+      email: "#formRegEmail",
+      phone: "#formRegPhone",
+      passw: "#formRegPassw",
+      passw_conf: "#formRegPassw2",
+      rules: "#formRegRules",
+      politic: "#formRegData"
+    },
+    // FORM LOGIN
+    login: {
+      id: "#formLogin",
+      email: "#formLoginEmail",
+      passw: "#formLoginPassw"
+    },
+  },
+};
+
+export default constants;
